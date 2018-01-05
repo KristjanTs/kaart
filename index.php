d7ccc0e86c3383b03cab8e8273c5420493938fc7
KristjanTs <kristjantsimmer@gmail.com>
2018-01-05 11:59:29 +0200

    .

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title>JSON1 and AJAX</title>
+  <title>Kaardirakendus</title>
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
@@ -23,7 +23,7 @@
     var mapOptions = {
       center: new google.maps.LatLng(58.382014, 26.728904),
       mapTypeId: google.maps.MapTypeId.ROADMAP,
-      zoom: 6
+      zoom: 17
     };
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
   };
