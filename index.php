dbc86b43b410f1507a7abfb33671e928cd87b4c2
KristjanTs <kristjantsimmer@gmail.com>
2018-01-14 19:54:11 +0200

    .

--- index.php
+++ index.php
@@ -8,27 +8,177 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
 
-  <link rel="stylesheet" href="http://kaart.1473350.ee/wp-content/themes/kaart-3/style.css" />
+  <link rel="stylesheet" href="style.css" />
 </head>
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAAs8Bmb6fzACSu3RDyMWV7JVxrXKp6D9o&callback=initMap" type="text/javascript"></script>
 
-<body onload="initialize();">
+<body>
   <div id="container">
-    <h1 id="pealkiri">Kaardirakendus</h1>
+    <div id="nav2">
+      <a href="https://linnamuuseum.tartu.ee/" ><img src="linnamuuseum.png" /></a>
+    </div>
     <div id="map"></div>
   </div>
 
   <script>
-  function initialize() {
-    var mapOptions = {
-      center: new google.maps.LatLng(58.382014, 26.728904),
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      zoom: 15,
-      disableDefaultUI: true,
-      zoomControl: true
+  function initMap() {
+    //Map style
+    var styledMapType = new google.maps.StyledMapType(
+      [
+        {elementType: 'geometry', stylers: [{color: '#ebe3cd'}]},
+        {elementType: 'labels.text.fill', stylers: [{color: '#523735'}]},
+        {elementType: 'labels.text.stroke', stylers: [{color: '#f5f1e6'}]},
+        {
+          featureType: 'administrative',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#c9b2a6'}]
+        },
+        {
+          featureType: 'administrative.land_parcel',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#dcd2be'}]
+        },
+        {
+          featureType: 'administrative.land_parcel',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#ae9e90'}]
+        },
+        {
+          featureType: 'landscape.natural',
+          elementType: 'geometry',
+          stylers: [{color: '#dfd2ae'}]
+        },
+        {
+          featureType: 'poi',
+          elementType: 'geometry',
+          stylers: [{color: '#dfd2ae'}]
+        },
+        {
+          featureType: 'poi',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#93817c'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'geometry.fill',
+          stylers: [{color: '#a5b076'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#447530'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry',
+          stylers: [{color: '#f5f1e6'}]
+        },
+        {
+          featureType: 'road.arterial',
+          elementType: 'geometry',
+          stylers: [{color: '#fdfcf8'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry',
+          stylers: [{color: '#f8c967'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#e9bc62'}]
+        },
+        {
+          featureType: 'road.highway.controlled_access',
+          elementType: 'geometry',
+          stylers: [{color: '#e98d58'}]
+        },
+        {
+          featureType: 'road.highway.controlled_access',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#db8555'}]
+        },
+        {
+          featureType: 'road.local',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#806b63'}]
+        },
+        {
+          featureType: 'transit.line',
+          elementType: 'geometry',
+          stylers: [{color: '#dfd2ae'}]
+        },
+        {
+          featureType: 'transit.line',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#8f7d77'}]
+        },
+        {
+          featureType: 'transit.line',
+          elementType: 'labels.text.stroke',
+          stylers: [{color: '#ebe3cd'}]
+        },
+        {
+          featureType: 'transit.station',
+          elementType: 'geometry',
+          stylers: [{color: '#dfd2ae'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'geometry.fill',
+          stylers: [{color: '#b9d3c2'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#92998d'}]
+        }
+      ],
+      {name: 'Styled Map'});
+
+
+
+      //Map options
+      var mapOptions = {
+        center: new google.maps.LatLng(58.382014, 26.728904),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        zoom: 15,
+        disableDefaultUI: true,
+        zoomControl: true,
+        mapTypeControlOptions: {
+          mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+          'styled_map']
+        }
+      };
+      //New map
+      var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+      map.mapTypes.set('styled_map', styledMapType);
+      map.setMapTypeId('styled_map');
+
+      addMarker(58.380790, 26.730808);
+      addMarker(58.384795, 26.728480);
+      addMarker(58.380668, 26.716430);
+
+      //Add marker
+      function addMarker(lat, lon){
+        var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(lat, lon),
+          map: map,
+          icon: "map.png"
+        });
+        marker.addListener("click", function(){
+          infoWindow.open(map, marker);
+        });
+      }
+
+      // popup - delete later
+      var infoWindow = new google.maps.InfoWindow({
+        content: "<h6>Lorem ipsum</h6>"
+      });
+
+
     };
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  };
-  </script>
-</body>
-</html>
+    </script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAAs8Bmb6fzACSu3RDyMWV7JVxrXKp6D9o&callback=initMap" type="text/javascript"></script>
+
+  </body>
+  </html>
