199d52deb652ab2ca724070ca3216db40632e9d3
KristjanTs <kristjantsimmer@gmail.com>
2018-02-21 08:20:42 +0200

    added meta data

--- functions.php
+++ functions.php
@@ -31,3 +31,44 @@ function my_acf_google_map_api( $api ){
 }
 
 add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
+
+add_action( 'cmb2_admin_init', 'cmb2_sample_metaboxes' );
+/**
+ * Define the metabox and field configurations.
+ */
+function cmb2_sample_metaboxes() {
+
+	// Start with an underscore to hide fields from custom fields list
+	$prefix = '_punktiga_seotud_pildid';
+
+	/**
+	 * Initiate the metabox
+	 */
+	$cmb = new_cmb2_box( array(
+		'id'            => 'punkti_pildid',
+		'title'         => __( 'Punktiga seotud pildid', 'cmb2' ),
+		'object_types'  => array( 'punkt', ), // Post type
+		'context'       => 'normal',
+		'priority'      => 'high',
+		'show_names'    => true, // Show field names on the left
+		// 'cmb_styles' => false, // false to disable the CMB stylesheet
+		// 'closed'     => true, // Keep the metabox closed by default
+	) );
+
+	// Regular text field
+	$cmb->add_field( array(
+		'name'       => __( 'Pildid', 'cmb2' ),
+		'desc'       => __( 'field description (optional)', 'cmb2' ),
+		'id'         => $prefix . 'file_list',
+		'type'       => 'file_list',
+    'preview_size' => array( 100, 100 ),
+		'show_on_cb' => 'cmb2_hide_if_no_cats', // function should return a bool value
+		// 'sanitization_cb' => 'my_custom_sanitization', // custom sanitization callback parameter
+		// 'escape_cb'       => 'my_custom_escaping',  // custom escaping callback parameter
+		// 'on_front'        => false, // Optionally designate a field to wp-admin only
+		// 'repeatable'      => true,
+	) );
+
+	// Add other metaboxes as needed
+
+}
